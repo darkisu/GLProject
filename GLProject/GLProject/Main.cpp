dbbf24ed81ce0b971072d5458c8828614911f1d7
Darkisu Su <darkisu@hotmail.com.tw>
2016-05-22 03:26:06 +0800

    1. showTexture function completed: To use it, showTexture_INIT function must be called. 2. the EBO values changed to GLuint: Previous bug was caused by using GLFloat as EBO type. Also, to draw a plain coordinate object, GL_DEPTH_TEST should be disabled.     -Previous bug: Things are correctly placed in buffers but nothing drawed. 3. Some minor changes to the comments 4. Test codes NOT removed yet.

--- GLProject/GLProject/Main.cpp
+++ GLProject/GLProject/Main.cpp
@@ -11,14 +11,18 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // flat square
-static const GLfloat UNIT_SQUARE[] = {
-	-1.0f, -1.0f, 0.0f, 0, 1,
-	1.0f, -1.0f, 0.0f, 1, 1,
-	-1.0f,  1.0f, 0.0f, 0, 0,
-	-1.0f,  1.0f, 0.0f, 0, 0,
-	1.0f, -1.0f, 0.0f, 1, 1,
-	1.0f,  1.0f, 0.0f, 1, 0
+static const GLfloat SQUARE_V[] = {
+	1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Top Right
+	1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // Bottom Left
+	-1.0f,  1.0f, 0.0f, 0.0f, 1.0f // Top Left 
 };
+
+static const GLuint SQUARE_I[] = {
+	0, 1, 3,  // First Triangle
+	1, 2, 3   // Second Triangle
+};
+
 
 // window initiation
 void initiate(int width, int height, string title);
@@ -48,22 +52,57 @@ GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
 
+//Show texture
+GLuint showtex_VBO, showtex_VAO, showtex_EBO;
+void showTexture_INIT()
+{
+
+	glGenVertexArrays(1, &showtex_VAO);
+	glGenBuffers(1, &showtex_VBO);
+	glGenBuffers(1, &showtex_EBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(showtex_VAO);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, showtex_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(SQUARE_V), SQUARE_V, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, showtex_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(SQUARE_I), SQUARE_I, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+}
+
+
 int main()
 {
 
 	initiate( screenWidth, screenHeight,"GLProject Result");
+
+	// Scene ID Variables
 	GLuint sponzaID, cubeID;
-	//Model TargetModel(".\\model\\nanosuit\\nanosuit.obj");
+
+	// Scenes
 	Scene TargetScene(".\\model\\sponza.obj", sponzaID);
-	//Model TargetModel2(".\\model\\cube\\cube.obj");
-	//Model TargetModel2(".\\model\\plane\\cube.obj");
-	Shader shader("./shader/defaultshader.vs", "./shader/defaultshader.fs");
-	Shader showTexShader("./shader/showTexture.vs", "./shader/showTexture.fs");
+
+	// Shaders
+	Shader shader("./shader/defaultshader.glvs", "./shader/defaultshader.glfs");
+	Shader showTexShader("./shader/showTexture.glvs", "./shader/showTexture.glfs");
 	//--------------test code--------------
 
 	GLuint renderTex, depthTex;
 	glGenTextures(1, &renderTex);
 	GLuint FBO;
+	glActiveTexture(GL_TEXTURE0);
 	glGenFramebuffers(1, &FBO);
 	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 
@@ -97,6 +136,7 @@ int main()
 	//-------------------------------------
 
 	//------Main Loop------//
+	showTexture_INIT();
 	while (!glfwWindowShouldClose(pWindow))
 	{
 		GLfloat currentFrameTime = glfwGetTime();
@@ -107,9 +147,6 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, 0);
 		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 		glViewport(0, 0, screenWidth, screenHeight);
-		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-
 
 		// Check and call events
 		glfwPollEvents();
@@ -122,10 +159,6 @@ int main()
 		// use shader
 		shader.Use();
 
-
-		
-
-		//------------------Render Main Model-----------------------------
 		// Camera setup
 		glUniform3fv(glGetUniformLocation(shader.Program, "ViewPos"), 1, glm::value_ptr(camera.Position));
 		glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 0.1f, 10000.0f);
@@ -139,8 +172,11 @@ int main()
 		TargetScene.Draw(shader);
 
 		//----------test code---------
-		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		//showTexture(renderTex,showTexShader);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, screenWidth, screenHeight);
+
+		showTexture(renderTex, showTexShader);
 
 
 		glfwSwapBuffers(pWindow);
@@ -245,27 +281,14 @@ void Do_Movement()
 void showTexture(const GLuint &textureID, Shader shader)
 {
 	shader.Use();
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	GLuint quad_vertexbuffer , VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glGenBuffers(1, &quad_vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(UNIT_SQUARE), UNIT_SQUARE, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glDisable(GL_DEPTH_TEST);
+	glBindVertexArray(showtex_VAO);
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	glUniform1f(glGetUniformLocation(shader.Program, "renderedTexture"), 0);
 
-	
-	glDrawArrays(GL_TRIANGLES, 0, 2);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
+	glEnable(GL_DEPTH_TEST);
 }
