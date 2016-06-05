40fec56a4f44a165add0628f74df8a2c159a963e
Darkisu Su <darkisu@hotmail.com.tw>
2016-06-05 17:51:11 +0800

    Revert "revert"
    This reverts commit f8b103850137e53ca96d8a2f06b9d1f70b454a31.

--- GLProject/GLProject/Main.cpp
+++ GLProject/GLProject/Main.cpp
@@ -2,6 +2,8 @@
 // Custom Class Includes
 #include "Scene.h"
 #include "Camera.h"
+#include "DeferredRenderer.h"
+#include "TextureShower.h"
 
 // GL Includes
 #include <GLEW\glew.h>
@@ -11,17 +13,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // flat square
-static const GLfloat SQUARE_V[] = {
-	1.0f,  1.0f, 0.0f, 1.0f, 1.0f, // Top Right
-	1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
-	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // Bottom Left
-	-1.0f,  1.0f, 0.0f, 0.0f, 1.0f // Top Left 
-};
 
-static const GLuint SQUARE_I[] = {
-	0, 1, 3,  // First Triangle
-	1, 2, 3   // Second Triangle
-};
 
 
 // window initiation
@@ -33,7 +25,6 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void Do_Movement();
 
 
-void showTexture(const GLuint &textureID, Shader shader);
 
 // Window
 GLuint screenWidth = 800;
@@ -52,52 +43,23 @@ GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
 
-//Show texture
-GLuint showtex_VBO, showtex_VAO, showtex_EBO;
-void showTexture_INIT()
-{
-
-	glGenVertexArrays(1, &showtex_VAO);
-	glGenBuffers(1, &showtex_VBO);
-	glGenBuffers(1, &showtex_EBO);
-	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-	glBindVertexArray(showtex_VAO);
-
-
-	glBindBuffer(GL_ARRAY_BUFFER, showtex_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(SQUARE_V), SQUARE_V, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, showtex_EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(SQUARE_I), SQUARE_I, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);
-
-}
-
-
 int main()
 {
 
 	initiate( screenWidth, screenHeight,"GLProject Result");
-	showTexture_INIT();
 	// Scene ID Variables
 	GLuint sponzaID, cubeID;
 
 	// Scenes
 	Scene TargetScene(".\\model\\sponza.obj", sponzaID);
-	TargetScene.def_INIT(screenWidth, screenHeight);
-	// Shaders
-	//Shader shader("./shader/defaultshader.glvs", "./shader/defaultshader.glfs");
-	Shader shader("./shader/deferredp1.glvs", "./shader/deferredp1.glfs");
+
+	 // Shaders
+	Shader shader("./shader/defaultshader.glvs", "./shader/defaultshader.glfs");
 	Shader showTexShader("./shader/showTexture.glvs", "./shader/showTexture.glfs");
+	Shader defshaderP1("./shader/deferredp1.glvs", "./shader/deferredp1.glfs");
+
+	// Deferred Renderer
+	DeferredRenderer renderer(&TargetScene, &defshaderP1, &shader, screenWidth, screenHeight);
 
 	//------Main Loop------//
 	while (!glfwWindowShouldClose(pWindow))
@@ -114,20 +76,17 @@ int main()
 		glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// use shader
-		shader.Use();
-
 		// Camera setup
 		glUniform3fv(glGetUniformLocation(shader.Program, "ViewPos"), 1, glm::value_ptr(camera.Position));
-		glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 0.1f, 10000.0f);
+		glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 1.0f, 10000.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-
-		// Draw the loaded model
+		
+		// Light Setup
 		TargetScene.setupPointOfLight(glm::vec3(0.0, 1000.0, 0.0), glm::vec3(1.0, 1.0, 1.0));
 
-		TargetScene.defDraw(shader);
+		renderer.step1Draw();
 
 		glfwSwapBuffers(pWindow);
 	}
@@ -226,19 +185,4 @@ void Do_Movement()
 		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (keys[GLFW_KEY_D])
 		camera.ProcessKeyboard(RIGHT, deltaTime);
-}
-
-void showTexture(const GLuint &textureID, Shader shader)
-{
-	shader.Use();
-	glDisable(GL_DEPTH_TEST);
-	glBindVertexArray(showtex_VAO);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glUniform1f(glGetUniformLocation(shader.Program, "renderedTexture"), 0);
-
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);
-	glEnable(GL_DEPTH_TEST);
 }
